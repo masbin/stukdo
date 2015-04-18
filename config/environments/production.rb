8f21ab4a259e43b17c3813fb3434cc281bbed3fd
masbin <bintarum@gmail.com>
2015-04-18 11:53:03 +0700

    change prod_rb static_asset true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   config.serve_static_assets = true
+  config.assets.compile = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
